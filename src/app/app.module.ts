56d31b0f4d8a903f1a8d717409818a8401634f28
Gustavo Leite <gustavo.leite170@gmail.com>
2022-02-24 22:00:54 -0300

    header being stylized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+/*=================Material-Imports=================*/
+
+import {MatIconModule} from '@angular/material/icon';
+
+/*=================Material-Imports=================*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
