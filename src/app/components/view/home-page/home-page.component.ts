51e0717a01d071d720578ee3db6d0c625bb8a6bc
Gustavo Leite <gustavo.leite170@gmail.com>
2022-03-06 21:46:48 -0300

    swiper added successfully

--- src/app/components/view/home-page/home-page.component.ts
+++ src/app/components/view/home-page/home-page.component.ts
@@ -1,8 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { SwiperOptions } from 'swiper';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { ChangeDetectorRef, NgZone, ViewChild } from "@angular/core";
+import { SwiperComponent } from "swiper/angular";
 
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+// import Swiper core and required components
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+  Zoom,
+  Autoplay,
+  Thumbs,
+  Controller
+} from "swiper";
+import { BehaviorSubject } from "rxjs";
+
+// install Swiper components
+SwiperCore.use([
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Virtual,
+  Zoom,
+  Autoplay,
+  Thumbs,
+  Controller
+]);
 
 @Component({
   selector: 'app-home-page',
@@ -11,17 +36,76 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 })
 export class HomePageComponent implements OnInit {
 
-  config: SwiperOptions = {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    navigation: true,
-    pagination: { clickable: true },
-    scrollbar: { draggable: true },
+  @ViewChild("swiperRef", { static: false }) swiperRef?: SwiperComponent;
+
+  thumbs: any;
+  slides$ = new BehaviorSubject<string[]>([""]);
+  constructor(private cd: ChangeDetectorRef, private ngZone: NgZone) {}
+  ngOnInit() {}
+
+  getSlides() {
+    this.slides$.next(
+      Array.from({ length: 600 }).map((el, index) => `Slide ${index + 1}`)
+    );
+  }
+
+  indexNumber = 1;
+  exampleConfig = { slidesPerView: 3 };
+  slidesPerView: number = 4;
+  pagination: any = false;
+
+  slides2 = ["slide 1", "slide 2", "slide 3"];
+  replaceSlides() {
+    this.slides2 = ["foo", "bar"];
+  }
+
+  togglePagination() {
+    if (!this.pagination) {
+      this.pagination = { type: "fraction" };
+    } else {
+      this.pagination = false;
+    }
+  }
+
+  navigation = false;
+  toggleNavigation() {
+    this.navigation = !this.navigation;
+  }
+
+  scrollbar: any = false;
+  toggleScrollbar() {
+    if (!this.scrollbar) {
+      this.scrollbar = { draggable: true };
+    } else {
+      this.scrollbar = false;
+    }
+  }
+  breakpoints = {
+    640: { slidesPerView: 2, spaceBetween: 20 },
+    768: { slidesPerView: 4, spaceBetween: 40 },
+    1024: { slidesPerView: 4, spaceBetween: 50 }
   };
 
-  constructor() { }
+  slides = Array.from({ length: 5 }).map((el, index) => `Slide ${index + 1}`);
+  virtualSlides = Array.from({ length: 600 }).map(
+    (el, index) => `Slide ${index + 1}`
+  );
 
-  ngOnInit(): void {
+  log(log: string) {
+    // console.log(string);
+  }
+
+
+  slidesEx = ["first", "second"];
+
+  onSlideChange(swiper: any) {
+    if (swiper.isEnd) {
+      // all swiper events are run outside of ngzone, so use ngzone.run or detectChanges to update the view.
+      this.ngZone.run(() => {
+        this.slidesEx = [...this.slidesEx, `added ${this.slidesEx.length - 1}`];
+      });
+      console.log(this.slidesEx);
+    }
   }
 
 }
