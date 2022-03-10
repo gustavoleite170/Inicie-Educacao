c39df15a19937e2ae7e0ad513d4ef006a5a221a0
Gustavo Leite <gustavo.leite170@gmail.com>
2022-03-10 13:35:51 -0300

    header responsive on process yet, but too close to be done

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./styles/header-general.component.css', './styles/header-left.component.css', './styles/header-right.component.css', './styles/header-responsive.component.css']
+  styleUrls: ['./styles/header-general.component.css', './styles/header-left.component.css', './styles/header-right.component.css', './styles/header-responsive.component.css', './styles/header-right-hamburguer.component.css']
 })
 export class HeaderComponent implements OnInit {
 
