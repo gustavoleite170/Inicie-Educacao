f3bd1020f4dc141bb935030c4fb5bf5aba74568b
Gustavo Leite <gustavo.leite170@gmail.com>
2022-03-10 00:12:06 -0300

    partners section responsive done

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header-general.component.css', './header-left.component.css', './header-right.component.css', './header-responsive.component.css']
 })
 export class HeaderComponent implements OnInit {
 
